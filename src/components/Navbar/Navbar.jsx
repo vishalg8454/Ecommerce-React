5924eb718f2c6ca64667e6dc8f78c8bdc8e0b402
Vishal Gaurav <lemeraemail@gmail.com>
2022-03-23 17:00:54 +0530

    feat: add navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import "../Navbar/navbar.css"
+import {Link} from "react-router-dom"
+
+export default function Navbar(){
+    return(
+        <nav className="navbar">
+            <div className="nav-container">
+                <Link className="nav-logo" to="/">BOOKSTOCK</Link>
+                <ul className="nav-list">
+                    <li className="nav-list-items">
+                        <div>
+                            <Link to="/"> <i className="badge-icon fas fa-heart nav-icon"></i></Link>
+                            <div className="badge-text">9</div>
+                        </div>
+                    </li>
+                    <li className="nav-list-items">
+                        <div>
+                            <Link to="/"><i className="badge-icon fas fa-shopping-cart nav-icon"></i></Link>
+                            <div className="badge-text">3</div>
+                        </div>
+                    </li>
+                    <li className="nav-list-items profile">
+                        <div>
+                            <Link to="/"><i className="fas fa-user nav-icon"></i></Link>
+                        </div>
+                    </li>
+                </ul>
+                <div className="nav-search-container">
+                    <input className="nav-search-text" type="text" />
+                    <button className="nav-search-button"><i className="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </nav>
+    )
+}
