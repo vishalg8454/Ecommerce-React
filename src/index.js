cddbbb0f10bda04be84a7abd22808a485fb3b1c6
Vishal Gaurav <lemeraemail@gmail.com>
2022-03-26 16:12:16 +0530

    feat: add ProductPage and it's route

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { makeServer } from "./server";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mockman from "mockman-js";
 import "mockman-js/dist/style.css";
-import {Homepage} from "./routes"
+import {Homepage,ProductPage} from "./routes"
 
 // Call make Server
 makeServer();
@@ -17,6 +17,7 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Homepage />}/>
+          <Route path="shop" element={<ProductPage />}/>
         </Route>
         <Route path="mock" element={<Mockman />} />
       </Routes>
---
+++ src/routes/productpage/ProductPage.jsx
@@ -0,0 +1,8 @@
+import "./product-page.css";
+
+const ProductPage = () => {
+  return (
+    <h1>Product Page</h1>      
+  );
+};
+export { ProductPage };
