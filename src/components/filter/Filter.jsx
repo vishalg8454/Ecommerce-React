49c02cb3fbe037e3fe85b2ed4a4e4b87517f8b9e
Vishal Gaurav <lemeraemail@gmail.com>
2022-03-26 16:38:52 +0530

    fix: fix Filter html attributes

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -15,47 +15,47 @@ const Filter = () =>{
             </div>
             <p className="filter-label">Category</p>
             <ul className="category-list">
-                <li Name="category-list-item">
+                <li className="category-list-item">
                     <input type="checkbox" name="category"/>
-                    <label for="fiction"> Fiction</label><br/>
+                    <label htmlFor="fiction"> Fiction</label><br/>
                 </li>
                 <li className="category-list-item">
                     <input type="checkbox" name="category"/>
-                    <label for="sci-fi"> Sci-Fi</label><br/>
+                    <label htmlFor="sci-fi"> Sci-Fi</label><br/>
                 </li>
                 <li className="category-list-item">
                     <input type="checkbox" name="category"/>
-                    <label for="novel"> Novel</label><br/>
+                    <label htmlFor="novel"> Novel</label><br/>
                 </li>
             </ul>
             <p className="filter-label">Rating</p>
             <ul className="category-list">
                 <li className="category-list-item">
                     <input type="radio" id="4star" name="rating" value="4"/>
-                    <label for="4star">{"4 Stars & above"}</label>
+                    <label htmlFor="4star">{"4 Stars & above"}</label>
                 </li>
                 <li className="category-list-item">
                     <input type="radio" id="4star" name="rating" value="3"/>
-                    <label for="3star">{"3 Stars & above"}</label>
+                    <label htmlFor="3star">{"3 Stars & above"}</label>
                 </li>
                 <li className="category-list-item">
                     <input type="radio" id="4star" name="rating" value="2"/>
-                    <label for="2star">{"2 Stars & above"}</label>
+                    <label htmlFor="2star">{"2 Stars & above"}</label>
                 </li>
                 <li className="category-list-item">
                     <input type="radio" id="4star" name="rating" value="1"/>
-                    <label for="1star">{"1 Stars & above"}</label>
+                    <label htmlFor="1star">{"1 Stars & above"}</label>
                 </li>
             </ul>
             <p className="filter-label">Sort by</p>
             <ul className="category-list">
                 <li className="category-list-item">
                     <input type="radio" id="low" name="sort"/>
-                    <label for="low">Price - Low to High</label>
+                    <label htmlFor="low">Price - Low to High</label>
                 </li>
                 <li className="category-list-item">
                     <input type="radio" id="high" name="sort"/>
-                    <label for="high">Price - High to Low</label>
+                    <label htmlFor="high">Price - High to Low</label>
                 </li>
             </ul>
         </aside>
