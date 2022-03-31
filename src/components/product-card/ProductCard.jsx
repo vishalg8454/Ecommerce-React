30e8fa4354033ec996939abb492294808e7a4b1e
Vishal Gaurav <lemeraemail@gmail.com>
2022-03-31 21:40:47 +0530

    refactor: destructuring in Products.jsx

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,17 +1,20 @@
 import "./product-card.css";
 
-const ProductCard = (props) => {
-  console.log(props.includeStock)
+const ProductCard = ({ image, title, price, includeStock }) => {
   return (
     <div
       className="card-container product-card-container"
-      style={props.includeStock ? { opacity: 1 } : { opacity: 0.2 }}
+      style={includeStock ? { opacity: 1 } : { opacity: 0.2 }}
     >
-      <img className="card-image product-card-img" src={props.image} alt="Item Image" />
-      <h6 className="card-title">{props.title}</h6>
-      <p className="card-description card-price">₹ {props.price}</p>
+      <img
+        className="card-image product-card-img"
+        src={image}
+        alt="Item Image"
+      />
+      <h6 className="card-title">{title}</h6>
+      <p className="card-description card-price">₹ {price}</p>
       <div className="card-btn-container">
-        <button className="card-btn card-btn-primary" >Add to Cart</button>
+        <button className="card-btn card-btn-primary">Add to Cart</button>
         <button className="card-btn card-btn-secondary">Save For Later</button>
         <i className="card-btn-icon far fa-heart"></i>
       </div>
