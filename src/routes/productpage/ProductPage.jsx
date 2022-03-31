165ae0add1cb97ae26d32009b2ad4979092c4bb2
Vishal Gaurav <lemeraemail@gmail.com>
2022-03-31 21:44:25 +0530

    refactor: optimize imports

--- src/routes/productpage/ProductPage.jsx
+++ src/routes/productpage/ProductPage.jsx
@@ -1,19 +1,18 @@
+import { useReducer, useEffect } from "react";
 import "./product-page.css";
-import { Filter, ProductCard } from "../../components";
 import { useProduct } from "../../context/product-context";
-import { useReducer } from "react";
-import { useEffect, useState } from "react";
+import { filterReducer } from "../../reducer/filter-reducer";
+import { Filter, ProductCard } from "../../components";
 import { filterProducts } from "../../utils/filter-products";
 import { includeProducts } from "../../utils/include-products";
 import { sortProducts } from "../../utils/sort-products";
-import { filterReducer } from "../../reducer/filter-reducer";
 
 const ProductPage = () => {
   const { productList } = useProduct();
 
-  useEffect(()=>{
-    document.title = "BookStock | Shop"
-  },[])
+  useEffect(() => {
+    document.title = "BookStock | Shop";
+  }, []);
 
   const [state, dispatch] = useReducer(filterReducer, {
     sortBy: "",
@@ -26,24 +25,25 @@ const ProductPage = () => {
     comic: false,
   });
 
-  const includedProducts = includeProducts(productList,state);
+  const includedProducts = includeProducts(productList, state);
   console.log(includedProducts);
 
-  const filteredProducts = filterProducts(includedProducts,state);
-  console.log("filter",filteredProducts);
-  
-  const sortedProducts = sortProducts(filteredProducts,state);
-  console.log("sorted",sortedProducts);
+  const filteredProducts = filterProducts(includedProducts, state);
+  console.log("filter", filteredProducts);
+
+  const sortedProducts = sortProducts(filteredProducts, state);
+  console.log("sorted", sortedProducts);
 
   const finalProducts = sortedProducts;
 
   return (
     <div className="product-page">
-      <Filter className="filter" state={state} dispatch={dispatch}/>
+      <Filter className="filter" state={state} dispatch={dispatch} />
       <main className="main">
         {finalProducts.map((product) => {
           return (
-            <ProductCard key={product._id}
+            <ProductCard
+              key={product._id}
               price={product.price}
               title={product.title}
               image={product.image}
